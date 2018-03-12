5dc455431382faec5bc770793b0d1f187c9fdcfb
cottr <cottr@DESKTOP-I649USP>
2018-03-12 22:41:07 +0000

    Removing the icons from the menu as they are the wrong size.

--- src/main/java/com/embeddediq/searchmonkey/Searchmonkey.java
+++ src/main/java/com/embeddediq/searchmonkey/Searchmonkey.java
@@ -286,6 +286,7 @@ public class Searchmonkey extends javax.swing.JFrame implements ActionListener, 
         saveAsMenuItem.setText("Save As ...");
         fileMenu.add(saveAsMenuItem);
 
+        exitMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F4, java.awt.event.InputEvent.ALT_MASK));
         exitMenuItem.setMnemonic('x');
         exitMenuItem.setText("Exit");
         exitMenuItem.addActionListener(new java.awt.event.ActionListener() {
@@ -321,7 +322,6 @@ public class Searchmonkey extends javax.swing.JFrame implements ActionListener, 
         jMenu1.setText("Tools");
 
         jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_T, java.awt.event.InputEvent.CTRL_MASK));
-        jMenuItem1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/074082-rounded-glossy-black-icon-alphanumeric-font-size.png"))); // NOI18N
         jMenuItem1.setMnemonic('T');
         jMenuItem1.setText("Test Expression");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
@@ -340,7 +340,7 @@ public class Searchmonkey extends javax.swing.JFrame implements ActionListener, 
         contentMenuItem.setText("Contents");
         helpMenu.add(contentMenuItem);
 
-        aboutMenuItem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/help.png"))); // NOI18N
+        aboutMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F1, 0));
         aboutMenuItem.setMnemonic('a');
         aboutMenuItem.setText("About");
         aboutMenuItem.addActionListener(new java.awt.event.ActionListener() {
